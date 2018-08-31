7f6e813454c63378decb2a7f08e9dacb7436254f
Carlton Stith <carlton215@gmail.com>
2018-08-31 16:28:50 -0400

    Completed the module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <li>{{book.title}}: {{book.author}}</li>
+</template>
+
+<script>
+export default {
+  name: 'BookItem',
+  props: ['book']
+}
+</script>
+
+<style lang="css">
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
